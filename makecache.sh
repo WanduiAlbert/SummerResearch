65b79965c26b445ed5d9cdb560370b56b23f0135
Albert Wandui <albertkamau16@gmail.com>
2015-07-13 14:41:37 -0500

    added a cache folders and updated the script

--- makecache.sh
+++ makecache.sh
@@ -3,4 +3,4 @@
 SITE='L1' #change this to H1 if working in the Hanford clusteri
 CHANNEL='GDS-CALIB_STRAIN_Omicron' # GSD calib strain
 
-ls /home/detchar/triggers/ER7/$SITE/$CHANNEL/*/* | lalapps_path2cache > hoft_Omicron_ER7.cache 
+ls /home/detchar/triggers/ER7/$SITE/$CHANNEL/*/* | lalapps_path2cache > /home/albert.wandui/caches/hoft_Omicron_ER7.cache 
