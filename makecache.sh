d889e3d8bca295ce12edcfe2aa6ff1bfad5422c4
Albert Wandui <albertkamau16@gmail.com>
2015-07-13 14:26:19 -0500

    added a script to make trigger caches

---
+++ makecache.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+SITE='L1' #change this to H1 if working in the Hanford clusteri
+CHANNEL='GDS-CALIB_STRAIN_Omicron' # GSD calib strain
+
+ls /home/detchar/triggers/ER7/$SITE/$CHANNEL/*/* | lalapps_path2cache > hoft_Omicron_ER7.cache 
