1c44e053f3adcae187a3aa8dd02fc793ae223f41
Albert Wandui <albertkamau16@gmail.com>
2015-07-14 11:57:40 -0500

    added command line args and changed histogram scales to log

--- plot_alltriggers.py
+++ plot_alltriggers.py
@@ -4,39 +4,32 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-L1data = np.load('ER7L1triggers.npz', 'r')
-H1data = np.load('ER7H1triggers.npz', 'r')
+#L1data = np.load('ER7L1triggers.npz', 'r')
+#H1data = np.load('ER7H1triggers.npz', 'r')
+import sys
 
-endtime =np.array(L1data['arr_0'])
-mass1 = np.array(L1data['arr_1'])
-mass2 = np.array(L1data['arr_2'])
-snr =  np.array(L1data['arr_3'])
-chisq = np.array(L1data['arr_4'])
+fn = sys.argv[1]
+gwdata = SnglInspiralTable.read(fn)
+endtime =np.array(gwdata.getColumnByName('end_time')[:]) +\
+    np.array(gwdata.getColumnByName('end_time_ns')[:]) * 1.0e-9
+mass1 = np.array(gwdata.getColumnByName('mass1'))
+mass2 = np.array(gwdata.getColumnByName('mass2'))
+snr =  np.array(gwdata.getColumnByName('snr'))
+chisq = np.array(gwdata.getColumnByName('chisq'))
 
 mtotal = mass1 + mass2
 eta = mass1 * mass2/ mtotal**2
 mchirp = eta**(3./5)*mtotal
 
-selection = snr >= 5.5
-plt.hist(snr[selection], bins=50)
-plt.xlabel('SNR')
-plt.ylabel('Number of events N')
-plt.title('L1, BBH triggers, ER7')
+selection = snr >= 0.0 
+plt.hist(snr[selection], bins=300)
+ax = plt.gca()
+ax.set_yscale('log', nonposy='clip')
+ax.set_xlabel('SNR')
+ax.set_ylabel('Number of events N')
+ax.set_xlim(1, 20)
+plt.title('H1, BBH triggers, ER7')
 plt.grid()
-plt.savefig('L1_snr.png')
+plt.savefig('H1_snr.png')
 
-plt.figure()
-plt.hist(mchirp[selection], bins=50)
-plt.xlabel('Chirp Mass $\mathcal{M}$ $(M_{\odot})$')
-plt.ylabel('Number of events N')
-plt.title('L1, BBH triggers, ER7')
-plt.grid()
-plt.savefig('L1_mchirp.png')
 
-plt.figure()
-plt.scatter(mchirp[selection], snr[selection])
-plt.xlabel('Chirp Mass $\mathcal{M}$ $(M_{\odot})$')
-plt.ylabel('SNR')
-plt.grid()
-plt.title('L1, BBH triggers, ER7')
-plt.savefig('L1_snr_vs_mchirp.png')
