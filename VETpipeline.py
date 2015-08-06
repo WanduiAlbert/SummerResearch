0ed359185c8b2c9199e98f6e176d47ea625fcb69
Albert Wandui <albertkamau16@gmail.com>
2015-08-06 13:49:05 -0700

    implemented functionality for reading in the data that we need

--- VETpipeline.py
+++ VETpipeline.py
@@ -18,18 +18,65 @@ bbhfile = sys.argv[2]
 omiccachedir = sys.argv[3]
 
 # Read in the segment file
-segments = SegmentList.read('/home/albert.wandui/detchar/ER7/jul13/%s_ER7_segments.txt' %ifo)
+segments = SegmentList.read('/home/albert.wandui/detchar'+\
+    '/ER7/jul13/%s_ER7_segments.txt' %ifo)
 
 # Read in the BBH triggers
 bbh_trigs = SnglInspiralTable.read(bbhfile)
 # We only want the triggers in the given segments
 bbh_trigs = bbh_trigs.vetoed(segments)
+#bbh_trigs.sort(key=lambda x: x.end_time + x.end_time_ns * 1.0e-9)
 
 # We need to extract the chirp mass and the end times for these triggers
 end_times = bbh_trigs.get_end()
 
 m1 = bbh_trigs.getColumnByName('mass1')
 m2 = bbh_trigs.getColumnByName('mass2')
-mchirp = (m1*m2)/(m1+m2)**2
 
+M = m1+m2
+eta = (m1*m2)/M**2
+mchirp = eta**(3./5)*M
+
+# Consider appending the Mchirp column to the SnglInspiralTable
+# ---------------------------------------------------------------------------- #
+
+# Read in all the Omicron caches
+# Also get an idea of the speed of the code when reading from cache file vs
+# letting vet get the data itself
+import time
+t0 = time.time()
+
+channels = channels.channellist() # Save all the channels in a python script
+Nchannels = len(channels)
+
+def omicron_triggers(channel, ifo, segments, cachefile):
+  with open(cachefile) as f:
+    mycache = Cache.fromfile(f)
+  # Let's try and catch failed reads
+  try:
+    triggers = get_triggers(ifo + ':' + channel, 'sngl_burst', segments,\
+        cache=mycache)
+  except:
+    print "No Omicron triggers read for channel %s" %channel
+    return None
+
+  return triggers
+
+# Use map to get all the data from the channels of interest at once
+# Make lists of all the parameters
+ifos = [ifo]*Nchannels
+segs = [segments]*Nchannels
+cachefiles = [cachefile]*Nchannels
+
+omic_triggers = map(channels, ifos, segs, cachefiles)
+
+t1  = time.time()
+print "All the Omicron triggers for %d channels loaded." %len(omic_triggers)
+print "This took %f seconds to run to completion" %(t1 - t0)
+
+# Get the peak times of the Omicron triggers
+omic_endtimes = map(lambda x: x.get_end(), omic_triggers)
+
+# ---------------------------------------------------------------------------- #
+# Now let's calculate the offsets between the BBH and Omicron triggers 
 
