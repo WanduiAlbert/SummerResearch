06eb850b8d7d4b79ce2f7bc43120967a28851556
Albert Wandui <albertkamau16@gmail.com>
2015-08-10 15:54:15 -0700

    moved the channels list to the script

--- VETpipeline.py
+++ VETpipeline.py
@@ -15,7 +15,23 @@ from gwpy.toolkits.vet import (get_triggers,get_segments,get_metric)
 
 import glob, os
 # Save all the channels in a python script
-from channels import channels
+channels = ["ASC-AS_B_RF36_I_YAW_OUT_DQ",
+"PEM-EY_MAG_EBAY_SUSRACK_QUAD_SUM_DQ",
+"LSC-POP_A_LF_OUT_DQ",
+"ASC-AS_B_RF45_Q_YAW_OUT_DQ",
+"OMC-ASC_POS_Y_OUT_DQ",
+"PEM-EX_TILT_VEA_FLOOR_T_DQ",
+"PEM-EY_TILT_VEA_FLOOR_T_DQ",
+"OMC-PZT2_MON_AC_OUT_DQ",
+"LSC-SRCL_OUT_DQ",
+"IMC-REFL_DC_OUT_DQ",
+"ALS-X_REFL_ERR_OUT_DQ",
+"PEM-CS_MAG_EBAY_SUSRACK_Y_DQ",
+"ASC-Y_TR_B_NSUM_OUT_DQ",
+"ASC-AS_B_RF45_Q_PIT_OUT_DQ",
+"SUS-OMC_M1_ISIWIT_T_DQ",
+"PSL-ISS_AOM_DRIVER_MON_OUT_DQ",
+"LSC-PRCL_OUT_DQ"]
 
 ifo = sys.argv[1]
 bbhdir = sys.argv[2]
